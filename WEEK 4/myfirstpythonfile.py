08d0132025e8efd9cccc9c218837763c3437210d
bernabejylemathiuepalad-stack <bernabejylemathiuepalad@gmail.com>
2025-10-30 12:58:44 +0400

    Add new text file in WEEK 4

--- WEEK 4/myfirstpythonfile.py
+++ WEEK 4/myfirstpythonfile.py
@@ -4,3 +4,10 @@ myAge = "18"
 
 print(myGreetings)
 print(myAge)
+
+combineGreetandAge1 = "hello world" + str (18)
+combineGreetandAge2 = myGreetings + str (myAge)
+
+print(combineGreetAndAge1)
+print(combineGreetingsAndAge2)
+
